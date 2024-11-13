34b22bc77eb6594dc419bd56bef942978f90b4eb
CHEN12205194 <1175790280@qq.com>
2024-11-14 00:24:15 +0800

    Update OSAP_001_AVLSet.h

--- OSAP_001_AVLSet.h
+++ OSAP_001_AVLSet.h
@@ -1,43 +1,61 @@
 #ifndef OSAPAVLSET_H
 #define OSAPAVLSET_H
+class Node {
+public:
+    Node(int val);
+    ~Node();
 
-class AVLTree
-{
+    int getKey() const;
+    void setKey(int val);
+    Node* getLeft() const;
+    void setLeft(Node* leftChild);
+    Node* getRight() const;
+    void setRight(Node* rightChild);
+    int getHeight() const;
+    void setHeight(int h);
+
 private:
-    // avl树节点
-    struct AVLNode
-    {
-        // 
-        int key;
-        int height;
-        AVLNode *left;
-        AVLNode *right;
+    int key;
+    Node* left;
+    Node* right;
+    int height;
+};
 
-        AVLNode(int k) : key(k), height(1), left(nullptr), right(nullptr) {}
-    };
-
-    AVLNode *root;
-
-    int height(AVLNode *node);
-    int balanceFactor(AVLNode *node);
-    void updateHeight(AVLNode *node);
-    AVLNode *rightRotate(AVLNode *y);
-    AVLNode *leftRotate(AVLNode *x);
-    AVLNode *insertNode(AVLNode *node, int key);
-    bool findNode(AVLNode *node, int key);
-    void clearTree(AVLNode *node);
-    int sizeNode(AVLNode *node);
-    bool emptyNode(AVLNode *node);
-
-
+class AVLTree {
 public:
     AVLTree();
     ~AVLTree();
-    void clear();
-    void insert(int key);
-    bool find(int key);
-    int size();
-    bool empty();
+
+    // 插入节点
+    int insert(int key);
+
+    // 查找节点
+    int find(int key);
+
+    // 检查树是否为空
+    bool empty() const;
+
+    // 获取树中节点数量
+    int size() const;
+
+    // 获取树的高度
+    int height() const;
+
+private:
+    Node* root;
+    int tree_size;
+
+    int getHeight(Node* N) const;
+    int max(int a, int b) const;
+    Node* rightRotate(Node* y);
+    Node* leftRotate(Node* x);
+    int getBalance(Node* N) const;
+    Node* insert(Node* node, int key, int& depth);
+    Node* find(Node* node, int key, int& depth) const;
+    void clear(Node* node);
+    int countNodes(Node* node) const;
+    int treeHeight(Node* node) const;
 };
+
 
 #endif
